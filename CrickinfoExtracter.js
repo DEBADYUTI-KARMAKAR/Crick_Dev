9cc8d673d29f9d0195e0bffaf53205fe39302472
Debadyuti Karmakar <debadyuti.karmakar@gmail.com>
2021-10-08 21:06:06 +0530

    Extract information

--- CrickinfoExtracter.js
+++ CrickinfoExtracter.js
@@ -19,5 +19,41 @@ responseKaPromise.then(function (response) {
 
     let dom = new jsdom.JSDOM(html);
     let document = dom.window.document;
-    console.log(document.title);
+    //console.log(document.title);
+
+    let matchScoreDivs = document.querySelectorAll("div.match-score-block");
+    let matches = [];
+
+    for(let i= 0;i< matchScoreDivs.length;i++){
+        let match = {
+            t1: "",
+            t2: "",
+            t1s: "",
+            t2s: "",
+            result: ""
+        };
+
+        
+        let teamParas = matchScoreDivs[i].querySelectorAll("div.name-detail > p.name");
+        match.t1 = teamParas[0].textContent;
+        match.t2 = teamParas[1].textContent;
+
+        let scoreSpans = matchScoreDivs[i].querySelectorAll("div.score-detail > span.score");
+        if (scoreSpans.length == 2) {
+            match.t1s = scoreSpans[0].textContent;
+            match.t2s = scoreSpans[1].textContent;
+        } else if (scoreSpans.length == 1) {
+            match.t1s = scoreSpans[0].textContent;
+            match.t2s = "";
+        } else {
+            match.t1s = "";
+            match.t2s = "";
+        }
+
+        let resultSpan = matchScoreDivs[i].querySelector("div.status-text > span");
+        match.result = resultSpan.textContent;
+
+        matches.push(match);
+    }
+    console.log(matches);
 })
